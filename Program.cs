cf2afc1b47ea6d9bf7f1b338719d7cb5c79c95c0
Александр Самарин <68380973+Westheimer2Kru@users.noreply.github.com>
2021-08-15 02:02:16 +0300

    Refactoring

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 
+
 // Учтены операторы + - * / ( ) 
 namespace Сalculator
 {
@@ -10,13 +11,82 @@ namespace Сalculator
         {
             Console.WriteLine("Ваше выражение:");
             string input = Console.ReadLine();
-            Console.WriteLine(Calculator.Calculate(input));
+            Calculator calc = new Calculator();
+            Console.WriteLine(calc.Calculate(input));
         }
+    }
+
+    interface IOperation
+    {
+        public int Priority { get; }
+        public float CalculateResult(float first, float second);
+    }
+
+    class Addition : IOperation
+    {
+        private static int priority = 1;
+        public int Priority
+            {
+                get => priority;
+            }
+        public float CalculateResult(float first, float second)
+        {
+            return first + second;
+        }
+    }
+
+    class Subtraction : IOperation
+    {
+        private static int priority = 1;
+        public int Priority
+        {
+            get => priority;
+        }
+        public float CalculateResult(float first, float second)
+        {
+            return second - first;
+        }
+    }
+
+    class Multiplication: IOperation
+    {
+        private static int priority = 2;
+        public int Priority
+        {
+            get => priority;
+        }
+        public float CalculateResult(float first, float second)
+        {
+            return first * second;
+        }
+    }
+
+    class Division: IOperation
+    {
+        private static int priority = 2;
+        public int Priority
+        {
+            get => priority;
+        }
+        public float CalculateResult(float first, float second)
+        {
+            return second / first;
+        }
+
     }
 
     class Calculator
     {
-        static public float Calculate(string input) // Решение с помощью 2 стеков
+        private Dictionary<char,IOperation> operatorTable = new Dictionary<char, IOperation>();
+
+        public Calculator()
+        {
+            operatorTable.Add('+', new Addition());
+            operatorTable.Add('-', new Subtraction());
+            operatorTable.Add('*', new Multiplication());
+            operatorTable.Add('/', new Division());
+        }
+        public float Calculate(string input) // Решение с помощью 2 стеков
         {
             float solve;
             input = input.Trim(' ');
@@ -28,33 +98,29 @@ namespace Сalculator
                 if (Char.IsDigit(input[i])) // если символ число, добавляем в первый стек
                 {
                     digits.Push((float)Char.GetNumericValue(input[i]));
-                }
-                    
+                }               
                 else if(IsOperator(input[i]))
                 {
                     if (operators.Count == 0) // в пустой стек просто добавляем оператор
                         operators.Push(input[i]);
                     else
                     {
-                      if(Priority(operators.Peek()) < Priority(input[i])) // при большем приоритете просто помещаем оператор в стек
+                        try
+                        {
+                            if (operatorTable[operators.Peek()].Priority < operatorTable[input[i]].Priority) // при большем приоритете просто помещаем оператор в стек
+                            {
+                                operators.Push(input[i]);
+                            }
+                            else // иначе вычисляем результат последних двух чисел и помещаем в первый стек
+                            {
+                                digits.Push(operatorTable[operators.Pop()].CalculateResult(digits.Pop(), digits.Pop()));
+                                i--;// шаг назад, чтобы вернуться к символу
+                            }
+                        }catch(KeyNotFoundException)
                         {
                             operators.Push(input[i]);
                         }
-                      else // иначе вычисляем результат последних двух чисел и помещаем в первый стек
-                        {
-                            try
-                            {
-                                digits.Push(Compute(digits.Pop(), digits.Pop(), operators.Pop()));
-                                i--;
-                            } catch (Exception e)
-                            {
-                                Console.WriteLine("Ошибка! Возможно вы допустили опечатку!");
-                                Console.WriteLine(e);
-                                return 0;
-                            }
-                            
-                             // шаг назад, чтобы вернуться к символу
-                        }
+                        
                     }
                 } else if (IsBracket(input[i])) // открывающую скобку добавляем во второй стек, закрывающая же вычисляет выражение в скобках
                 {
@@ -69,8 +135,8 @@ namespace Сalculator
                         }
                         while (operators.Peek() != '(')
                         {
-                            digits.Push(Compute(digits.Pop(), digits.Pop(), operators.Pop()));
-                            if(operators.Count == 0)
+                            digits.Push(operatorTable[operators.Pop()].CalculateResult(digits.Pop(), digits.Pop()));
+                            if (operators.Count == 0)
                             {
                                 Console.WriteLine("Пропущена открывающая скобка");
                                 return 0;
@@ -81,61 +147,19 @@ namespace Сalculator
                     }
                 }
             }
+            while (operators.Count > 0) // если по окончанию строки остались действия, вычисляем их
+                  digits.Push(operatorTable[operators.Pop()].CalculateResult(digits.Pop(), digits.Pop()));
 
-            try
-            {
-                while (operators.Count > 0) // если по окончанию строки остались действия, вычисляем их
-                    digits.Push(Compute(digits.Pop(), digits.Pop(), operators.Pop()));
-            }catch(Exception e)
-            {
-                Console.WriteLine("Произошла ошибка! Возможно, вы упустили скобку");
-                Console.WriteLine(e);
-                return 0;
-            }
             solve = digits.Pop(); // в стеке должно остаться одно число - наш ответ
             return solve;
         }
-
-        static private float Compute(float num1, float num2, char oper)
-        {
-            if(oper == '(')
-            {
-                Console.WriteLine("Пропущена закрывающая скобка!");
-                return 0;
-            }
-            if (oper == '+')
-                return num1 + num2;
-            else if (oper == '-')
-                return num2 - num1;
-            else if (oper == '*')
-                return num1 * num2;
-            else
-                return num2 / num1;
-        }
-
         static private bool IsOperator(char oper)
         {
-            if ("+-*/".Contains(oper))
-                return true;
-            return false;
+            return "+-*/".Contains(oper);
         }
         static private bool IsBracket(char oper)
         {
-            if ("()".Contains(oper))
-                return true;
-            return false;
+            return "()".Contains(oper);
         }
-        static private int Priority(char oper)
-        {
-            return oper switch
-            {
-                '+' => 1,
-                '-' => 1,
-                '*' => 2,
-                '/' => 2,
-                _ => 0,
-            };
-        }
-
     }
 }
