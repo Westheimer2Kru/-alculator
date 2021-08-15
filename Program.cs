0e216f7f4d393b205acd81d86d8716b838b368a5
Александр Самарин <68380973+Westheimer2Kru@users.noreply.github.com>
2021-08-16 01:23:33 +0300

    Exceptions and Tests

--- Program.cs
+++ Program.cs
@@ -12,7 +12,14 @@ namespace Сalculator
             Console.WriteLine("Ваше выражение:");
             string input = Console.ReadLine();
             Calculator calc = new Calculator();
-            Console.WriteLine(calc.Calculate(input));
+            try
+            {
+                Console.WriteLine(calc.Calculate(input));
+            }catch(Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            
         }
     }
 
@@ -24,7 +31,7 @@ namespace Сalculator
 
     class Addition : IOperation
     {
-        private static int priority = 1;
+        private static readonly int priority = 1;
         public int Priority
             {
                 get => priority;
@@ -37,7 +44,7 @@ namespace Сalculator
 
     class Subtraction : IOperation
     {
-        private static int priority = 1;
+        private static readonly int priority = 1;
         public int Priority
         {
             get => priority;
@@ -50,7 +57,7 @@ namespace Сalculator
 
     class Multiplication: IOperation
     {
-        private static int priority = 2;
+        private static readonly int priority = 2;
         public int Priority
         {
             get => priority;
@@ -63,7 +70,7 @@ namespace Сalculator
 
     class Division: IOperation
     {
-        private static int priority = 2;
+        private static readonly int priority = 2;
         public int Priority
         {
             get => priority;
@@ -75,91 +82,113 @@ namespace Сalculator
 
     }
 
-    class Calculator
-    {
-        private Dictionary<char,IOperation> operatorTable = new Dictionary<char, IOperation>();
-
-        public Calculator()
+    public class Calculator
         {
-            operatorTable.Add('+', new Addition());
-            operatorTable.Add('-', new Subtraction());
-            operatorTable.Add('*', new Multiplication());
-            operatorTable.Add('/', new Division());
-        }
-        public float Calculate(string input) // Решение с помощью 2 стеков
-        {
-            float solve;
-            input = input.Trim(' ');
-            Stack<float> digits = new Stack<float>();
-            Stack<char> operators = new Stack<char>();
+            private Dictionary<char,IOperation> operatorTable = new Dictionary<char, IOperation>();
 
-            for(int i=0; i<input.Length; i++)
+            public Calculator()
             {
-                if (Char.IsDigit(input[i])) // если символ число, добавляем в первый стек
+                operatorTable.Add('+', new Addition());
+                operatorTable.Add('-', new Subtraction());
+                operatorTable.Add('*', new Multiplication());
+                operatorTable.Add('/', new Division());
+            }
+            public float Calculate(string input) // Решение с помощью 2 стеков
+            {
+                float solve;
+                input = Remover.RemoveSpaces(input);
+                Stack<float> digits = new Stack<float>();
+                Stack<char> operators = new Stack<char>();
+
+                for(int i=0; i<input.Length; i++)
                 {
-                    digits.Push((float)Char.GetNumericValue(input[i]));
-                }               
-                else if(IsOperator(input[i]))
-                {
-                    if (operators.Count == 0) // в пустой стек просто добавляем оператор
-                        operators.Push(input[i]);
-                    else
+                    if (Char.IsDigit(input[i])) // если символ число, добавляем в первый стек
                     {
-                        try
+                        digits.Push((float)Char.GetNumericValue(input[i]));
+                    }               
+                    else if(IsOperator(input[i]))
+                    {
+                        if (operators.Count == 0) // в пустой стек просто добавляем оператор
+                            operators.Push(input[i]);
+                        else
                         {
-                            if (operatorTable[operators.Peek()].Priority < operatorTable[input[i]].Priority) // при большем приоритете просто помещаем оператор в стек
+                            try
+                            {
+                                if (operatorTable[operators.Peek()].Priority < operatorTable[input[i]].Priority) // при большем приоритете просто помещаем оператор в стек
+                                {
+                                    operators.Push(input[i]);
+                                }
+                                else // иначе вычисляем результат последних двух чисел и помещаем в первый стек
+                                {
+                                    digits.Push(operatorTable[operators.Pop()].CalculateResult(digits.Pop(), digits.Pop()));
+                                    i--;// шаг назад, чтобы вернуться к символу
+                                }
+                            }catch(KeyNotFoundException)
                             {
                                 operators.Push(input[i]);
                             }
-                            else // иначе вычисляем результат последних двух чисел и помещаем в первый стек
-                            {
-                                digits.Push(operatorTable[operators.Pop()].CalculateResult(digits.Pop(), digits.Pop()));
-                                i--;// шаг назад, чтобы вернуться к символу
-                            }
-                        }catch(KeyNotFoundException)
-                        {
-                            operators.Push(input[i]);
-                        }
                         
-                    }
-                } else if (IsBracket(input[i])) // открывающую скобку добавляем во второй стек, закрывающая же вычисляет выражение в скобках
-                {
-                    if (input[i] == '(')
-                        operators.Push(input[i]);
-                    else
-                    {
-                        if (operators.Count == 0)
-                        {
-                            Console.WriteLine("Пропущена открывающая скобка");
-                            return 0;
                         }
-                        while (operators.Peek() != '(')
+                    } 
+                    else if (IsBracket(input[i])) // открывающую скобку добавляем во второй стек, закрывающая же вычисляет выражение в скобках
+                    {
+                        if (input[i] == '(')
+                            operators.Push(input[i]);
+                        else
                         {
-                            digits.Push(operatorTable[operators.Pop()].CalculateResult(digits.Pop(), digits.Pop()));
                             if (operators.Count == 0)
                             {
-                                Console.WriteLine("Пропущена открывающая скобка");
-                                return 0;
+                                throw new InvalidOperationException();
                             }
-                        }
-                        operators.Pop();
+                            while (operators.Peek() != '(')
+                            {
+                                digits.Push(operatorTable[operators.Pop()].CalculateResult(digits.Pop(), digits.Pop()));
+                                if (operators.Count == 0)
+                                {
+                                    throw new InvalidOperationException();
+                                }
+                            }
+                            operators.Pop();
                             
+                        }
+                    }
+                    else
+                    {
+                        throw new FormatException();
                     }
                 }
-            }
-            while (operators.Count > 0) // если по окончанию строки остались действия, вычисляем их
-                  digits.Push(operatorTable[operators.Pop()].CalculateResult(digits.Pop(), digits.Pop()));
+                try
+                {
+                    while (operators.Count > 0) // если по окончанию строки остались действия, вычисляем их
+                       digits.Push(operatorTable[operators.Pop()].CalculateResult(digits.Pop(), digits.Pop()));
+                }catch(KeyNotFoundException)
+                {
+                    throw new InvalidOperationException();
+                }
+            
 
-            solve = digits.Pop(); // в стеке должно остаться одно число - наш ответ
-            return solve;
+                solve = digits.Pop(); // в стеке должно остаться одно число - наш ответ
+                return solve;
+            }
+            static private bool IsOperator(char oper)
+            {
+                return "+-*/".Contains(oper);
+            }
+            static private bool IsBracket(char oper)
+            {
+                return "()".Contains(oper);
+            }
         }
-        static private bool IsOperator(char oper)
+    public class Remover
+    {
+        static public string RemoveSpaces(string input)
         {
-            return "+-*/".Contains(oper);
-        }
-        static private bool IsBracket(char oper)
-        {
-            return "()".Contains(oper);
+                input = input.Replace(" ", string.Empty);
+                input = input.Trim().Replace(" ", string.Empty);
+
+                return input;
         }
     }
+    
+    
 }
